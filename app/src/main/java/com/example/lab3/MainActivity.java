4bc80e72bf00ac05851e3aa8acf1625da6b61d60
Emily Haines <haineseb@g.cofc.edu>
2021-03-11 17:56:27 -0500

    create views

--- app/src/main/java/com/example/lab3/MainActivity.java
+++ app/src/main/java/com/example/lab3/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.lab3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView = findViewById(R.id.class_schedule);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] classes = {"Personal Finance", "Public Speaking", "CITA Seminar", "Web Design", "CS Seminar"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, classes);
+
+        listView = findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
+
+
     }
+
+
 }
